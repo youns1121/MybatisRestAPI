dae01bbf5a4eb65751068e1edfe536376dffeb75
younsikMin <minys94@daum.net>
2021-10-27 17:56:58 +0900

    ServiceImpl

---
+++ src/main/java/com/mybatisrestapi/service/impl/BoardServiceImpl.java
@@ -0,0 +1,28 @@
+package com.mybatisrestapi.service.impl;
+
+import com.mybatisrestapi.dao.BoardDao;
+import com.mybatisrestapi.dto.BoardDto;
+import com.mybatisrestapi.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardDao boardDao;
+
+    @Override
+    public List<BoardDto> getBoardList() {
+        return boardDao.getBoardList();
+    }
+
+    @Override
+    public int newBoard(BoardDto boardDto) {
+        return boardDao.newBoard(boardDto);
+    }
+
+
+}
