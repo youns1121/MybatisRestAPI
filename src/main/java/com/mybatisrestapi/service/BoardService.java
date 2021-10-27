3c5a049f37b5ef0d1efed1d327ed7f57ec9773a8
younsikMin <minys94@daum.net>
2021-10-27 17:56:33 +0900

    Service

---
+++ src/main/java/com/mybatisrestapi/service/BoardService.java
@@ -0,0 +1,14 @@
+package com.mybatisrestapi.service;
+
+import com.mybatisrestapi.dto.BoardDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface BoardService {
+
+    List<BoardDto> getBoardList();
+
+    int newBoard(BoardDto boardDto);
+}
