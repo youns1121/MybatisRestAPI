2f919f4aea518a9f1117578ff4590e7f3945d17a
youns <minys94@daum.net>
2021-10-27 23:10:33 +0900

    Swagger 어노테이션 추가

--- src/main/java/com/mybatisrestapi/controller/BoardController.java
+++ src/main/java/com/mybatisrestapi/controller/BoardController.java
@@ -2,25 +2,30 @@ package com.mybatisrestapi.controller;
 
 import com.mybatisrestapi.dto.BoardDto;
 import com.mybatisrestapi.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "Board API", tags = "Board API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/board")
+@RequestMapping("/v2/api/board")
 public class BoardController {
 
     private final BoardService boardService;
 
 
+    @ApiOperation(value = "게시판 조회", notes = "게시판을 조회합니다.")
     @GetMapping("/list")
     public List<BoardDto> getBoard(){
 
         return boardService.getBoardList();
     }
 
+    @ApiOperation(value = "게시판 생성", notes = "게시판을 생성합니다.")
     @PostMapping("/create")
     public Integer createBoard(@RequestBody BoardDto boardDto){
 
--- src/main/java/com/mybatisrestapi/dto/BoardDto.java
+++ src/main/java/com/mybatisrestapi/dto/BoardDto.java
@@ -1,16 +1,22 @@
 package com.mybatisrestapi.dto;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class BoardDto {
 
+    @ApiModelProperty(value = "게시판 아이디")
     private Long id;
+    @ApiModelProperty(value = "게시판 제목")
     private String title;
+    @ApiModelProperty(value = "게시판 내용")
     private String content;
+    @ApiModelProperty(value = "게시판 작성자")
     private String author;
+    @ApiModelProperty(value = "게시판 삭제여부", hidden = true)
     private String del_yn = "N";
 
 
