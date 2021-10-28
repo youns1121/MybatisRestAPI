940ec47770eb3a49574202b418828b91dbf903c3
younsikMin <minys94@daum.net>
2021-10-28 17:57:25 +0900

    생성시간, 수정시간 (미구현)

--- src/main/java/com/mybatisrestapi/dto/BoardDto.java
+++ src/main/java/com/mybatisrestapi/dto/BoardDto.java
@@ -1,23 +1,43 @@
 package com.mybatisrestapi.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class BoardDto {
 
-    @ApiModelProperty(value = "게시판 아이디")
+    @ApiModelProperty(value = "게시판 아이디", hidden = true)
     private Long id;
     @ApiModelProperty(value = "게시판 제목")
     private String title;
     @ApiModelProperty(value = "게시판 내용")
-    private String content;
+    private String contents;
     @ApiModelProperty(value = "게시판 작성자")
     private String author;
+
+    @ApiModelProperty(value = "생성시간")
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "수정시간")
+    private LocalDateTime updateTime;
+
+
     @ApiModelProperty(value = "게시판 삭제여부", hidden = true)
-    private String delYn = "N";
+    private String delYn;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 
 
@@ -25,12 +45,10 @@ public class BoardDto {
     }
 
     @Builder
-    public BoardDto(Long id, String title, String content, String author, String delYn) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.author = author;
-        this.delYn = delYn;
+    public void createBoard(BoardDto boardDto){
+        this.title = boardDto.getTitle();
+        this.contents = boardDto.getContents();
+        this.author = boardDto.getAuthor();
     }
 
 
