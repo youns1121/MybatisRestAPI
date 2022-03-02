fe16965e6b5429df505468858c4f39b80a1472d3
youns <minys94@daum.net>
2022-03-03 02:11:54 +0900

    BoardServiceImpl

--- src/main/java/com/mybatisrestapi/service/impl/BoardServiceImpl.java
+++ src/main/java/com/mybatisrestapi/service/impl/BoardServiceImpl.java
@@ -1,13 +1,12 @@
 package com.mybatisrestapi.service.impl;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.mybatisrestapi.repository.BoardRepository;
+
 import com.mybatisrestapi.dto.BoardDto;
+import com.mybatisrestapi.vo.BoardVo;
+import com.mybatisrestapi.repository.BoardRepository;
 import com.mybatisrestapi.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,47 +16,20 @@ public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
 
+
     @Override
-    @Transactional
-    public List<BoardDto> getBoardList() {
-        return boardRepository.getBoardList();
+    public Long createBoard(BoardVo boardVo) {
+
+        Long success = boardRepository.createBoard(boardVo);
+
+        return success;
     }
 
     @Override
-    @Transactional
-    public Long newBoard(BoardDto boardDto) {
-        return boardRepository.newBoard(boardDto);
+    public List<BoardDto> findAllByBoard() {
+
+        List<BoardDto> boardList = boardRepository.findAllByBoard();
+
+        return boardList;
     }
-
-    @Override
-    @Transactional
-    public BoardDto getBoard(Long param) { // 특정 게시판 조회
-        return boardRepository.getBoard(param);
-    }
-
-    @Override
-    @Transactional
-    public Long editBoard(BoardDto boardDto) { // 수정
-        boardDto.setId(boardDto.getId());
-
-        return boardRepository.editBoard(boardDto);
-
-    }
-
-    @Override
-    @Transactional
-    public void delBoard(Long id) {
-        boardRepository.delBoard(id);
-    }
-
-    @Override
-    public PageInfo<BoardDto> findAll(int pageNo, int pageSize) {
-
-        PageHelper.startPage(pageNo, pageSize);
-        List<BoardDto> boardDtoList = boardRepository.getBoardList();
-        PageInfo<BoardDto> pageInfo = new PageInfo<>(boardDtoList);
-        return pageInfo;
-    }
-
-
 }
