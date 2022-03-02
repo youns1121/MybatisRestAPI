b239ab7d6bd5f1be44d88de7996243398a0b9e46
youns <minys94@daum.net>
2022-03-03 02:11:38 +0900

    BoardService

--- src/main/java/com/mybatisrestapi/service/BoardService.java
+++ src/main/java/com/mybatisrestapi/service/BoardService.java
@@ -1,26 +1,15 @@
 package com.mybatisrestapi.service;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageInfo;
 import com.mybatisrestapi.dto.BoardDto;
+import com.mybatisrestapi.vo.BoardVo;
 
 import java.util.List;
 
 
 public interface BoardService {
 
-    List<BoardDto> getBoardList();
+    Long createBoard(BoardVo boardVo);
 
-    Long newBoard(BoardDto boardDto);
-
-    BoardDto getBoard(Long param);
-
-    Long editBoard(BoardDto boardDto); // 수정
-
-    void delBoard(Long id); //삭제, delYn = 'Y';
-
-    PageInfo<BoardDto> findAll(int pageNo, int pageSize); //페이징;
-
-
+    List<BoardDto> findAllByBoard();
 
 }
